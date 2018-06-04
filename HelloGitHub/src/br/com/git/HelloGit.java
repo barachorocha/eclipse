0b8a3961ac9c842cdd4de315aece02142595807a
barachorocha <baracho.rocha@gmail.com>
2018-06-04 19:36:31 -0300

    Arquivos adicionados via upload

---
+++ HelloGitHub/src/br/com/git/HelloGit.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package br.com.git;
+
+/**
+ * Integration example eclipse github.
+ * 
+ * @author Baracho
+ * 
+ * @since Jun 04, 2018
+ * 
+ * @version 1.0
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GitHub.");
+
+	}
+
+}
